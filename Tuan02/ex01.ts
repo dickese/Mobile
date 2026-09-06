4723009c19770eaab5d9c0ea288e358db484d57d
dickese <151850393+dickese@users.noreply.github.com>
2026-09-06 14:44:09 +0700

    Delete Tuan02 directory

--- Tuan02/ex01.ts
+++
@@ -1,8 +0,0 @@
-// Ex 01: Create a Promise that returns "Hello Async" after 2 seconds.
-const helloAsync: Promise<string> = new Promise<string>((resolve) => {
-  setTimeout(() => resolve("Hello Async"), 2000);
-});
-
-helloAsync.then((message: string) => console.log(message));
-
-export { helloAsync };
